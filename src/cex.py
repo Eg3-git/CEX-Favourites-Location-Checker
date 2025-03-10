7e2d10c10a5384a55490c47a8af54ef4d0c767e4
Eg3-git <ewangreig15@gmail.com>
2025-03-10 19:17:31 +0000

    App allows user to sign in then goes to favourites to gather items

---
+++ src/cex.py
@@ -0,0 +1,57 @@
+import platform
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+
+def infer_default_browser():
+    if platform.system() == "Windows":
+        return webdriver.Chrome()
+    elif platform.system() == "Darwin":
+        return webdriver.Chrome()
+    else:
+        return webdriver.Firefox()
+    
+def check_element_presence(driver, locator, timeout=3):
+    try:
+        WebDriverWait(driver, timeout).until(
+            lambda d: len(d.find_elements(*locator)) > 0
+        )
+        return True
+    except:
+        return False
+    
+driver = infer_default_browser()
+
+driver.get("https://uk.webuy.com")
+WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "header-account-button")))
+locate_cookie_popup = (By.ID, "onetrust-accept-btn-handler")
+
+if check_element_presence(driver, locate_cookie_popup):
+    cookie_accept = driver.find_element(*locate_cookie_popup)
+    cookie_accept.click()
+
+login_span = driver.find_element(By.XPATH, "//span[contains(@class, 'header-account-button')]")
+login_span.click()
+
+print("Please sign in")
+WebDriverWait(driver, 300).until(expected_conditions.url_changes(driver.current_url))
+
+target_page = "https://uk.webuy.com/user/account?tab=favourites&page=1&sortBy=most-recent"
+driver.get(target_page)
+WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "cx-account-card-grid")))
+product_on_page = driver.find_elements(By.XPATH, "//a[contains(@class, 'line-clamp')]")
+product_links = [item.get_attribute("outerHTML") for item in product_on_page]
+max_pages = int(driver.find_elements(By.XPATH, "//ul/li/span[@class='page-link']")[-1].text)
+
+if max_pages > 1:
+    for i in range(2, max_pages+1):
+        driver.get(f"https://uk.webuy.com/user/account?tab=favourites&page={i}&sortBy=most-recent")
+        WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "cx-account-card-grid")))
+        product_on_page = driver.find_elements(By.XPATH, "//a[contains(@class, 'line-clamp')]")
+        product_links.extend([item.get_attribute("outerHTML") for item in product_on_page])
+
+for p in product_links:
+    print(p)
+
+driver.quit()
