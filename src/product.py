bceaf349ab6f1b3b0f54134cafa0b097e9ecfa9b
Eg3-git <ewangreig15@gmail.com>
2025-03-14 21:23:55 +0000

    Able to now load favourites and local stores from files, and return stores for each item

--- src/product.py
+++ src/product.py
@@ -1,3 +1,4 @@
+import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -11,26 +12,39 @@ def check_element_presence(driver, locator, timeout=3):
         return True
     except:
         return False
-    
-driver = webdriver.Chrome()
 
-driver.get("https://uk.webuy.com/product-detail?id=5026555431835&categoryName=PLAYSTATION5-GAMES&superCatName=GAMING&title=&queryID=59B82598F0384E22B02FEA6D6CFCEE2A&position=4")
-WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "cx-link")))
+with open("data/stores.txt") as f:
+    local_stores = f.read().splitlines()
+    print(local_stores)
+
+with open("data/favourites.csv") as f:
+    favourites_writer = csv.reader(f)
+    favourites_list = list(favourites_writer)
+
+driver = webdriver.Chrome()
+driver.get("https://uk.webuy.com")
+WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "header-account-button")))
 locate_cookie_popup = (By.ID, "onetrust-accept-btn-handler")
 
 if check_element_presence(driver, locate_cookie_popup):
     cookie_accept = driver.find_element(*locate_cookie_popup)
     cookie_accept.click()
 
-check_stock_in_store = driver.find_element(By.XPATH, "//span[contains(@class, 'cx-link')]")
-check_stock_in_store.click()
+for item, url in favourites_list:
+    driver.get(url)
+    WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.XPATH, "//span[text()='Check stock in store' or text()='Pick up unavailable']")))
+    check_stock_in_store = driver.find_element(By.XPATH, "//span[text()='Check stock in store' or text()='Pick up unavailable']")
+    if check_stock_in_store.text == 'Pick up unavailable':
+        print(f"{item} - Out of stock")
+    else:
+        check_stock_in_store.click()
 
-WebDriverWait(driver, 3).until(expected_conditions.element_to_be_clickable((By.CLASS_NAME, "icon-text-button")))
-all_stores_button = driver.find_element(By.XPATH, "//button[.//span[text()='All Stores']]")
-all_stores_button.click()
+        WebDriverWait(driver, 5).until(expected_conditions.element_to_be_clickable((By.CLASS_NAME, "icon-text-button")))
+        all_stores_button = driver.find_element(By.XPATH, "//button[.//span[text()='All Stores']]")
+        all_stores_button.click()
 
-WebDriverWait(driver, 3).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "store-card")))
-all_stores = driver.find_elements(By.CSS_SELECTOR, "a.store-card")
-print(len(all_stores))
-locations = [store.find_element(By.CSS_SELECTOR, "span:first-of-type").text for store in all_stores]
-print(locations)
+        WebDriverWait(driver, 5).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "store-card")))
+        all_stores = driver.find_elements(By.CSS_SELECTOR, "a.store-card")
+        locations = [store.find_element(By.CSS_SELECTOR, "span:first-of-type").text for store in all_stores]
+
+        print(f"{item} - {[s for s in locations if any(sub in s for sub in local_stores)]}")
